9fa1648a511a898d8b5469f33ee5c39a3c2361b9
saniya koyal <dodoprogramming019@gmail.com>
2025-05-27 18:54:32 +0530

    Add files via upload

---
+++ 1task1.py
@@ -0,0 +1,10 @@
+a=float(input("Enter first number:"))
+b=float(input("Enter Second number:"))
+
+print("The addition is",a+b)
+print("The subtraction is",a-b)
+print("The Multiplication is",a*b)
+if b!=0:
+     print("The division  is",a/b)
+else:
+     print("The division is:Cannot divide by zero")     
---
+++ 1task2.py
@@ -0,0 +1,4 @@
+name = input("Enter your name:")
+last = input("Enter your last:")
+
+print("Hello, "+name+" "+last+"! happesit birthday to you")
