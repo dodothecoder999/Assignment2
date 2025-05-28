3aea29a9cfc10899167ad23f6b61d93523863478
saniya koyal <dodoprogramming019@gmail.com>
2025-05-28 16:06:41 +0530

    Update 1task2.py

--- 1task2.py
+++ 1task2.py
@@ -2,3 +2,9 @@ name = input("Enter your name:")
 last = input("Enter your last:")
 
 print("Hello, "+name+" "+last+"! happesit birthday to you")
+
+OUTPUT
+PS C:\Users\SANIYA\OneDrive\Desktop\dodothecoder> & "C:/Program Files/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/1task2.py
+Enter your name:saniya
+Enter your last:koyal
+Hello, saniya koyal! happesit birthday to you
