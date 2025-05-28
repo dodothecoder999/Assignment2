01b43dbfb4d7b74347dfb06ea8f1f35020e6d634
saniya koyal <dodoprogramming019@gmail.com>
2025-05-28 16:05:28 +0530

    Update 1task1.py

--- 1task1.py
+++ 1task1.py
@@ -5,4 +5,13 @@ print("The addition is",a+b)
 print("The subtraction is",a-b)
 print("The Multiplication is",a*b)
 print("The division  is",a/b)
+
+output
+PS C:\Users\SANIYA\OneDrive\Desktop\dodothecoder> & "C:/Program Files/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/1task1.py
+Enter first number:7
+Enter Second number:9
+The addition is 16.0
+The subtraction is -2.0
+The Multiplication is 63.0
+The division  is 0.7777777777777778
           
