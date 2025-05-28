f32312828abe57ee4650f683d86fbb9410febcc6
saniya koyal <dodoprogramming019@gmail.com>
2025-05-28 14:13:41 +0530

    Add files via upload

---
+++ 1task1.py
@@ -0,0 +1,8 @@
+a=float(input("Enter first number:"))
+b=float(input("Enter Second number:"))
+
+print("The addition is",a+b)
+print("The subtraction is",a-b)
+print("The Multiplication is",a*b)
+print("The division  is",a/b)
+          
